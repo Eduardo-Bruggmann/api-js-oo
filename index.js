d77153d9198175c6fa7471f3a5355640cca28318
Eduardo Bruggmann <eduardo_bruggmann150@estudante.sesisenai.org.br>
2026-08-04 10:16:38 -0300

    feat: add user validations

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import router from './src/routes/routes.js'
+import * as middlewares from './src/middlewares/middlewares.js'
+
+const { errorMiddleware } = middlewares
 
 const PORT = 5000
 const app = express()
@@ -8,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(router)
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,5 @@
 import Service from './Service.js'
+import ValidationError from '../errors/ValidationError.js'
 
 export default class UserService extends Service {
   constructor(userRepository) {
@@ -8,13 +9,31 @@ export default class UserService extends Service {
   validate(user) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    return (
-      user &&
-      user.name &&
-      user.email &&
-      user.name.length <= 100 &&
-      user.email.length <= 100 &&
-      emailRegex.test(user.email)
-    )
+    if (!user) throw new ValidationError('Payload is required.')
+
+    if (Object.keys(user).length > 2)
+      throw new ValidationError('Too many fields in the payload.')
+
+    if (Object.keys(user).some(key => !['name', 'email'].includes(key)))
+      throw new ValidationError('Invalid field in the payload.')
+
+    if (!user.name) throw new ValidationError('Name is required.')
+
+    if (user.name.length > 100)
+      throw new ValidationError(
+        'Name must be less than or equal to 100 characters.',
+      )
+
+    if (!user.email) throw new ValidationError('Email is required.')
+
+    if (user.email.length > 100)
+      throw new ValidationError(
+        'Email must be less than or equal to 100 characters.',
+      )
+
+    if (!emailRegex.test(user.email))
+      throw new ValidationError('Email is invalid.')
+
+    return true
   }
 }
