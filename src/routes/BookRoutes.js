fb8a6e7a2830aa322530c953b3b39ced177b1d43
Eduardo-Bruggmann <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2026-08-08 13:00:46 -0300

    fix: missing imports

--- src/routes/BookRoutes.js
+++ src/routes/BookRoutes.js
@@ -1,13 +1,15 @@
 import { prisma } from '../../database/prisma.js'
 import BookController from '../controllers/BookController.js'
 import BookRepository from '../repositories/BookRepository.js'
+import BookRepository from '../repositories/AuthorRepository.js'
 import BookService from '../services/BookService.js'
 import BaseRouter from './BaseRouter.js'
 
 export default class BookRouter extends BaseRouter {
   constructor() {
     const bookRepository = new BookRepository(prisma)
-    const bookService = new BookService(bookRepository)
+    const authorRepository = new AuthorRepository(prisma)
+    const bookService = new BookService(bookRepository, authorRepository)
     const bookController = new BookController(bookService)
 
     super({
