306523641eb0cabec759f139394cb9d43564e323
Eduardo-Bruggmann <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2026-08-09 16:32:09 -0300

    feat: frontend initialized

---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello, World!')
+
+  document.getElementById('app').innerHTML = `
+    <h1 class="text-9xl">Hello, World!</h1>
+  `
+})
